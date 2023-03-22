203935ae9cb23ebc2fb1e6c0af02effad6bdc124
Mac_Developer <araozangel842@gmail.com>
2023-03-22 20:37:41 +0200

    Add constants and action types

---
+++ src/interfaces/constants.ts
@@ -0,0 +1,26 @@
+import { Product } from './ProductType'
+
+export const GET_PRODUCTS = 'GET_PRODUCTS'
+
+export const LOADING = 'LOADING'
+export const STOP_LOADING = 'STOP_LOADING'
+export const ERROR = 'ERROR'
+
+//Action types for product
+export type LoadingAction = {
+  type: typeof LOADING
+}
+export type StopLoadingAction = {
+  type: typeof STOP_LOADING
+}
+export type ErrorAction = {
+  type: typeof ERROR
+  payload: string | null
+}
+
+export type GetProductsAction = {
+  type: typeof GET_PRODUCTS
+  payload: Product[]
+}
+
+export type ProductActionTypes = GetProductsAction | LoadingAction | StopLoadingAction | ErrorAction
