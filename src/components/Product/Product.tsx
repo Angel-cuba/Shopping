ecbe5ccead2e577b29093c9e5d12f9c952db6f86
Mac_Developer <araozangel842@gmail.com>
2023-03-23 23:00:47 +0200

    Add styles to single product item

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import { Product } from '../../interfaces/ProductType'
+import { CartIcon } from '../Cart/Cart'
 
 const ProductItem = ({ product }: { product: Product }) => {
-  console.log('🚀 ~ file: Product.tsx:5 ~ ProductItem ~ product:', product)
+  const [itemAddedCart, setAddedCart] = React.useState<boolean>(false)
+
+  const handleAddToCart = () => {
+    setAddedCart(!itemAddedCart)
+  }
+
   return (
-    <div key={product.id}>
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
+    <div key={product.id} className="products__content__item">
+      <div className="products__content__item--name">
+        <h3>{product.name.slice(0, 12)}</h3>
+        <div className="products__content__item--name--icon" onClick={handleAddToCart}>
+          <CartIcon itemAddedToCart={itemAddedCart} />
+        </div>
+      </div>
+      <div className="products__content__item--image">
+        <img
+          src={product.image}
+          alt={product.name}
+          style={{
+            width: '90%',
+            height: '200px',
+            borderRadius: '5px'
+          }}
+        />
+        <p className="products__content__item--image--price">£{product.price}</p>
+      </div>
+      <div className="products__content__item--buttons">
+        <div className="products__content__item--buttons--buy">Buy</div>
+        <div className="products__content__item--buttons--details">Details</div>
+      </div>
     </div>
   )
 }
