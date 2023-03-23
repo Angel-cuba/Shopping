b2e91002487b9d3ce22bcc1d724314b9c4bc43fd
Mac_Developer <araozangel842@gmail.com>
2023-03-24 00:22:27 +0200

    Add route for single product

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import ProductById from '../components/Product/ProductById'
 import Home from './Home'
 import Login from './Login'
 
@@ -8,6 +9,8 @@ const Navigation = () => {
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<h1>404</h1>} />
+      <Route path="/product/:id" element={<ProductById />} />
     </Routes>
   )
 }
