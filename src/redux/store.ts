c9c2935340c8a4b47e364e54fd85b9a22a5ae46d
Mac_Developer <araozangel842@gmail.com>
2023-03-24 22:10:27 +0200

    Implementing store with both reducers combined

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { legacy_createStore as createStore, applyMiddleware } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import { CartState } from '../interfaces/cart/constants'
+import { ProductState } from '../interfaces/products/constants'
+import { initialCartState } from './reducers/CartReducer'
+import { initialProductState } from './reducers/ProductReducer'
+import { rootReducers } from './reducers'
 
-import ProductReducer from './reducers/ProductReducer'
+export type RootState = {
+  cart: CartState
+  products: ProductState
+}
+const rootState: RootState = {
+  cart: initialCartState,
+  products: initialProductState
+}
 
-export const store = configureStore({
-  reducer: {
-    items: ProductReducer
-  }
-})
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export const store = createStore(rootReducers, rootState, applyMiddleware(thunk))
 export type AppDispatch = typeof store.dispatch
