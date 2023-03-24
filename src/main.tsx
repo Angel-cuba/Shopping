f1ffe4515b7c6da521eb444e48206fb77dc4567e
Mac_Developer <araozangel842@gmail.com>
2023-03-24 17:34:02 +0200

    Test google login

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from './App'
-import './styles/index.scss'
-import { store } from './redux/store'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import App from './App'
+import { store } from './redux/store'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </GoogleOAuthProvider>
 )
--- src/router/Login.tsx
+++ src/router/Login.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { GoogleLogin } from '@react-oauth/google'
 
 const Login = () => {
-  return <div>Login</div>
+  const handleResponse = (response: unknown) => {
+    console.log(response)
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      <GoogleLogin onSuccess={handleResponse} />
+    </div>
+  )
 }
 
 export default Login
