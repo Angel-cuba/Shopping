12c612575d5a1f10ec41687da9acefade07cd2fe
Mac_Developer <araozangel842@gmail.com>
2023-03-25 22:29:08 +0200

    Implement add and remove method

--- src/redux/reducers/CartReducer.ts
+++ src/redux/reducers/CartReducer.ts
@@ -8,16 +8,52 @@ export const initialCartState: CartState = {
 export default function CartReducer(state = initialCartState, action: AnyAction) {
   switch (action.type) {
     case ADD_TO_CART: {
+      if (state.itemInCart === null) {
+        return {
+          ...state,
+          itemInCart: [{ ...action.payload, quantity: 1 }]
+        }
+      }
+      const product = state.itemInCart.find((item) => item.id === action.payload.id)
+      if (product) {
+        return {
+          ...state,
+          itemInCart: state.itemInCart.map((item) => {
+            if (item.id === product.id) {
+              return { ...item, quantity: item.quantity ? item.quantity + 1 : 1 }
+            }
+            return item
+          })
+        }
+      }
       return {
         ...state,
-        cart: action.payload
+        itemInCart: [...state.itemInCart, { ...action.payload, quantity: 1 }]
       }
     }
     case REMOVE_FROM_CART: {
-      return {
-        ...state,
-        cart: action.payload
+      if (state.itemInCart === null) {
+        return state
       }
+      const product = state.itemInCart.find((item) => item.id === action.payload.id)
+      if (product) {
+        if (product.quantity === 1) {
+          return {
+            ...state,
+            itemInCart: state.itemInCart.filter((item) => item.id !== product.id)
+          }
+        }
+        return {
+          ...state,
+          itemInCart: state.itemInCart.map((item) => {
+            if (item.id === product.id) {
+              return { ...item, quantity: item.quantity ? item.quantity - 1 : 1 }
+            }
+            return item
+          })
+        }
+      }
+      return state
     }
     default:
       return state
