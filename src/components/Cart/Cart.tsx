6024876708e6ee6b1058ce61a099b143133ba778
Mac_Developer <araozangel842@gmail.com>
2023-03-26 05:18:29 +0300

    Ongoing work.
    Adding styles to items in cart. Missing the total price and the checkout button.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from '../../redux/store'
 import { addToCart, removeFromCart } from '../../redux/actions/CartActions'
 import { Product } from '../../interfaces/products/ProductType'
+import SingleProduct from './ProductInCart'
 
 export const NavbarIcon = () => {
   const [openCart, setOpenCart] = React.useState(false)
@@ -17,8 +18,21 @@ export const NavbarIcon = () => {
   const showCartItems = () => {
     setOpenCart(!openCart)
   }
+
+  const toPay = () => {
+    let total = 0
+    itemInCart?.forEach((item) => {
+      total += item.price * item.quantity
+    })
+    return total.toFixed(2)
+  }
+
   return (
     <div className="navbar-cart">
+      <div className={openCart ? 'navbar-cart__total' : 'navbar-cart__total--hidden'}>
+        <p className="navbar-cart__total--text">Total to pay:</p>
+        <p className="navbar-cart__total--price">${toPay()}</p>
+      </div>
       <div className="navbar-cart__icon" onClick={showCartItems}>
         <AddShoppingCartIcon
           style={{
@@ -26,8 +40,21 @@ export const NavbarIcon = () => {
           }}
         />
       </div>
-      <div className="navbar-cart__amount">{!itemInCart ? emptyCart : itemInCart.length}</div>
-      <div className={openCart ? 'navbar-cart__cart' : 'navbar-cart__cart--show'}></div>
+      <div
+        className={
+          !itemInCart
+            ? 'navbar-cart__amount'
+            : itemInCart && itemInCart?.length > 9
+            ? 'navbar-cart__amount--over'
+            : 'navbar-cart__amount'
+        }>
+        {!itemInCart ? emptyCart : itemInCart.length}
+      </div>
+      <div className={openCart ? 'navbar-cart__cart' : 'navbar-cart__cart--hidden'}>
+        {itemInCart?.map((item) => (
+          <SingleProduct item={item} key={item.id} />
+        ))}
+      </div>
     </div>
   )
 }
