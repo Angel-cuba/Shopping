8e90fcd8d8f4c3ade9b546c76105bcbf1ba38574
Mac_Developer <araozangel842@gmail.com>
2023-03-27 10:32:11 +0300

    Refactoring add item to cart functionality
    Now same product can be added to cart choosing different size and color

--- src/redux/reducers/CartReducer.ts
+++ src/redux/reducers/CartReducer.ts
@@ -15,17 +15,46 @@ export default function CartReducer(state = initialCartState, action: AnyAction)
         }
       }
       const product = state.itemInCart.find((item) => item.id === action.payload.id)
+      const time = new Date().getTime()
       if (product) {
-        return {
-          ...state,
-          itemInCart: state.itemInCart.map((item) => {
-            if (item.id === product.id) {
-              return { ...item, quantity: item.quantity ? item.quantity + 1 : 1 }
-            }
-            return item
-          })
+        if (product.variant !== action.payload.variant && product.sizes === action.payload.sizes) {
+          const newItem = { ...action.payload, id: action.payload.id + time, quantity: 1 }
+          return {
+            ...state,
+            itemInCart: [...state.itemInCart, newItem]
+          }
+        }
+        if (product.variant === action.payload.variant && product.sizes !== action.payload.sizes) {
+          const newItem = { ...action.payload, id: action.payload.id + time, quantity: 1 }
+          return {
+            ...state,
+            itemInCart: [...state.itemInCart, newItem]
+          }
+        }
+        if (product.variant !== action.payload.variant && product.sizes !== action.payload.sizes) {
+          const newItem = { ...action.payload, id: action.payload.id + time, quantity: 1 }
+          return {
+            ...state,
+            itemInCart: [...state.itemInCart, newItem]
+          }
+        }
+        if (product.variant === action.payload.variant && product.sizes === action.payload.sizes) {
+          return {
+            ...state,
+            itemInCart: state.itemInCart.map((item) => {
+              if (
+                item.id === product.id &&
+                item.variant === action.payload.variant &&
+                item.sizes === action.payload.sizes
+              ) {
+                return { ...item, quantity: item.quantity ? item.quantity + 1 : 1 }
+              }
+              return item
+            })
+          }
         }
       }
+
       return {
         ...state,
         itemInCart: [...state.itemInCart, { ...action.payload, quantity: 1 }]
