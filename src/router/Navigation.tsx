7cb749d37e6fdf8a10a6976e7d0aba6492d4430b
Mac_Developer <araozangel842@gmail.com>
2023-03-27 14:23:52 +0300

    Enable routes to authenticaded user

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router'
 import ProductById from '../components/Product/ProductById'
+import { RootState } from '../redux/store'
 import Home from './Home'
 import Login from './Login'
 
 const Navigation = () => {
+  const { user } = useSelector((state: RootState) => state.userLogged)
+  console.log('🚀 ~ file: Navigation.tsx:11 ~ Navigation ~ user:', user)
+  const loginRoutes = [
+    { path: '', element: <Login /> },
+    { path: '/', element: <Login /> },
+    { path: '/login', element: <Login /> },
+    { path: '/*', element: <Login /> }
+  ]
   return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<h1>404</h1>} />
-      <Route path="/product/:id" element={<ProductById />} />
-    </Routes>
+    <>
+      {!user ? (
+        <Routes>
+          {loginRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/product/:id" element={<ProductById />} />
+        </Routes>
+      )}
+    </>
   )
 }
 
