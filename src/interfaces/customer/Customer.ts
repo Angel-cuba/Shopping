ad8fab327ff786d62c31460a7145aac729eebeba
Mac_Developer <araozangel842@gmail.com>
2023-03-28 22:08:03 +0300

    Add Customer type

---
+++ src/interfaces/customer/Customer.ts
@@ -0,0 +1,16 @@
+export type ICustomer = {
+  id: number
+  name: string
+  picture: string
+  lastSeen: string
+  orders: number
+  total_spents: string
+  last_purchase: Purchase
+  news: boolean
+  segments: string
+}
+
+type Purchase = {
+  date: string
+  time: string
+}
