9c1081d68d47c238ee21a9ed7617af425084bae0
Mac_Developer <araozangel842@gmail.com>
2023-03-30 15:08:38 +0300

    Ongoing work.
    Fetch user data and next user shipping history.
    Add shipping history to user profile.
    Work on checkout fetching cart items and adding payment method.

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import SingleProduct from '../../components/Cart/Product/SingleProduct'
+import { RootState } from '../../redux/store'
+import './Checkout.scss'
 
 const Checkout = () => {
-  return <div>Checkout</div>
+  const { itemInCart } = useSelector((state: RootState) => state.cart)
+
+  return (
+    <div className="checkout-view">
+      <div className="checkout-view__cart">
+        {itemInCart?.map((item) => (
+          <SingleProduct item={item} key={item.id} />
+        ))}
+      </div>
+      <div className="checkout-view__payment-method">payment method</div>
+    </div>
+  )
 }
 
 export default Checkout
--- src/pages/User/Profile.tsx
+++ src/pages/User/Profile.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
 
 const Profile = () => {
-  return <div>Profile</div>
+  const { user } = useSelector((state: RootState) => state.userLogged)
+  console.log('🚀 ~ file: Profile.tsx:7 ~ Profile ~ user:', user)
+  return (
+    <div className="profile">
+      <div className="profile__container">
+        <div className="profile__container__header">
+          <h1>Welcome {user?.role === 'ADMIN' ? 'administrator' : ''}</h1>
+
+          <div className="profile__container__header__user">
+            <div className="profile__container__header__user__avatar">
+              <img
+                src={user?.picture}
+                alt={user?.name}
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  objectFit: 'cover'
+                }}
+              />
+            </div>
+            <div className="profile__container__header__user__info">
+              <h2>{user?.name}</h2>
+              <p>{user?.email}</p>
+            </div>
+          </div>
+        </div>
+        <div className="profile__container__history">
+          <h2>History</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
