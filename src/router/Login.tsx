c34c00c408f8b153fbfa08cf7bb4534ecc77317f
Mac_Developer <araozangel842@gmail.com>
2023-03-31 17:51:55 +0300

    Refactor login layout

--- src/router/Login.tsx
+++ src/router/Login.tsx
@@ -5,24 +5,59 @@ import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../redux/store'
 import { login } from '../redux/actions/UserAction'
 import { UserType } from '../interfaces/user/UserType'
+import { Input } from '../components/Input/Input'
+import './Login.scss'
 
 const Login = () => {
+  const [userLogin, setUserLogin] = React.useState({
+    email: '',
+    password: ''
+  })
   const dispatch = useDispatch<AppDispatch>()
 
   const handleResponse = (response: any) => {
     if (response.credential) {
-      const token = `${response.credential}`
       localStorage.setItem('token', response.credential)
       const userDecoded: UserType = jwtDecode(response.credential)
       dispatch(login(userDecoded))
     }
   }
+  const handlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setUserLogin({ ...userLogin, [name]: value })
+  }
   return (
-    <div>
-      <h1>Login</h1>
-      <GoogleLogin onSuccess={handleResponse} />
+    <div className="login-view">
+      <div className="login-view__container">
+        <Input
+          name="Email"
+          onChange={handlerChange}
+          value={userLogin.email}
+          placeholder="youremail@gmail.com"
+          style={inputStyle}
+        />
+        <Input
+          name="Password"
+          onChange={handlerChange}
+          value={userLogin.password}
+          placeholder="**********"
+          style={inputStyle}
+        />
+        <div className="login-view__container__button">Login</div>
+        <GoogleLogin onSuccess={handleResponse} />
+      </div>
     </div>
   )
 }
 
 export default Login
+
+const inputStyle = {
+  width: '230px',
+  height: '40px',
+  fontSize: '20px',
+  padding: '10px',
+  borderRadius: '5px',
+  border: '1px solid #ccc',
+  marginBottom: '10px'
+}
