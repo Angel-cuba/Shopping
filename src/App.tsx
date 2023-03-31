2a737b45bc0203eb12350c68a057c13df5f48bac
Mac_Developer <araozangel842@gmail.com>
2023-03-31 23:03:19 +0300

    testing Netlify deployment

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Here to find your future!!!
         </a>
       </header>
     </div>
