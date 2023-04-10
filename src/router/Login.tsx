eecc0fe22c0fca0b1da85b0cd873248d109f39cd
Mac_Developer <araozangel842@gmail.com>
2023-04-10 19:55:17 +0300

    Ongoing work.
    Get email and password for login.

--- src/router/Login.tsx
+++ src/router/Login.tsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
-import jwtDecode from 'jwt-decode'
-import { useDispatch } from 'react-redux'
-import { AppDispatch } from '../redux/store'
-import { login } from '../redux/actions/UserAction'
-import { UserType } from '../interfaces/user/UserType'
-import { Input } from '../components/Input/Input'
-import './styles/Login.scss'
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../redux/store';
+import { login } from '../redux/actions/UserAction';
+import { UserType } from '../interfaces/user/UserType';
+import { Input } from '../components/Input/Input';
+import './styles/Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [userLogin, setUserLogin] = React.useState({
-    email: '',
-    password: ''
-  })
-  const dispatch = useDispatch<AppDispatch>()
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
 
   const handleResponse = (response: any) => {
     if (response.credential) {
-      localStorage.setItem('token', response.credential)
-      const userDecoded: UserType = jwtDecode(response.credential)
-      dispatch(login(userDecoded))
+      localStorage.setItem('token', response.credential);
+      const userDecoded: UserType = jwtDecode(response.credential);
+      dispatch(login(userDecoded));
+      navigate('/');
     }
-  }
+  };
   const handlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setUserLogin({ ...userLogin, [name]: value })
-  }
+    if (e.target.name === 'Email') {
+      setEmail(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  };
+  const handlerSubmit = (e: React.FormEvent ) => {
+    e.preventDefault();
+    console.log('submit', email, password);
+  };
   return (
     <div className="login-view">
       <div className="login-view__container">
         <Input
           name="Email"
           onChange={handlerChange}
-          value={userLogin.email}
+          value={email}
           placeholder="youremail@gmail.com"
           style={inputStyle}
         />
         <Input
           name="Password"
           onChange={handlerChange}
-          value={userLogin.password}
+          value={password}
           placeholder="**********"
           style={inputStyle}
         />
-        <div className="login-view__container__button">Login</div>
-        <GoogleLogin onSuccess={handleResponse} />
+        <button className="login-view__container__button" onClick={handlerSubmit}>Login</button>
+        <GoogleLogin onSuccess={handleResponse} onError={() => console.log('Failed')} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const inputStyle = {
   width: '230px',
@@ -59,5 +68,5 @@ const inputStyle = {
   padding: '10px',
   borderRadius: '5px',
   border: '1px solid #ccc',
-  marginBottom: '10px'
-}
+  marginBottom: '10px',
+};
