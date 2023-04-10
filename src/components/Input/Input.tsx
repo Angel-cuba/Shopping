9d5d7640810df77c9d166a4abc83a348224e8677
Mac_Developer <araozangel842@gmail.com>
2023-04-10 23:33:43 +0300

    Add props and styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,13 +8,21 @@ type InputProps = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   style?: { [key: string]: string };
   className?: string;
-  // error: string,
-  // touched: boolean,
   message?: string;
   admin?: boolean;
+  profile?: boolean;
 };
 
-export const Input = ({ type, name, placeholder, value, onChange, style, admin }: InputProps) => {
+export const Input = ({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  style,
+  admin,
+  profile,
+}: InputProps) => {
   return (
     <div
       style={{
@@ -25,9 +33,18 @@ export const Input = ({ type, name, placeholder, value, onChange, style, admin }
         width: admin ? '100%' : '200px',
         marginLeft: admin ? '0' : '20px',
         padding: admin ? '10px' : '0',
+        borderRadius: '5px',
       }}
     >
-      <label style={{ color: '#313131', width: '80%', fontSize: '18px' }} htmlFor={name}>
+      <label
+        style={{
+          color: '#313131',
+          width: profile ? '40%' : '80%',
+          fontSize: '18px',
+          fontWeight: 'bold',
+        }}
+        htmlFor={name}
+      >
         {name[0].toLocaleUpperCase() + name.slice(1)}
       </label>
       <input
