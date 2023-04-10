b95964cd63f30dc6ab64dcd07d94745d22c93c31
Mac_Developer <araozangel842@gmail.com>
2023-04-10 20:05:59 +0300

    Add properties for user type

--- src/redux/actions/UserAction.ts
+++ src/redux/actions/UserAction.ts
@@ -2,13 +2,31 @@ import { LOGGED_IN, LOGGED_OUT } from '../../interfaces/user/constants'
 import { UserType } from '../../interfaces/user/UserType'
 
 export function login(user: UserType) {
-  const newUser = {
-    ...user,
+  const newUser: UserType = {
+    email: user.email,
+    given_name: user.given_name,
+    family_name: user.family_name,
+    picture: user.picture,
+    name: user.name,
+    password: '',
+    phone: '',
+    address: '',
+    city: '',
+    postalCode: '',
+    country: '',
+    cardHolder: '',
+    paymentType: '',
+    provider: '',
+    accountNumber: '',
+    expiryDate: ''
+  }
+  const userToStorage = {
+    ...newUser,
     role: user.email === 'araozangel842@gmail.com' ? 'ADMIN' : 'USER'
   }
   return {
     type: LOGGED_IN,
-    payload: newUser
+    payload: userToStorage
   }
 }
 
