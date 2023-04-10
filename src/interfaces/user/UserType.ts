3f9c4e3691dc886dfa06c7611b92be67c4e819ef
Mac_Developer <araozangel842@gmail.com>
2023-04-10 19:57:27 +0300

    Add more properties for user type.

--- src/interfaces/user/UserType.ts
+++ src/interfaces/user/UserType.ts
@@ -1,18 +1,20 @@
 export type UserType = {
-  aud: string
-  azp: string
-  email_verified: boolean
-  exp: number
   email: string
   given_name: string
   family_name: string
   picture: string
-  iat: number
-  iss: string
-  jti: string
   name: string
-  nbf: number
-  sub: string
+  password: string
+  phone: string | null
+  address: string | null
+  city: string | null
+  postalCode: string | null
+  country: string | null
+  cardHolder: string | null
+  paymentType: string | null
+  provider: string | null
+  accountNumber: string | null
+  expiryDate: string | null
   role?: Role
 }
 
