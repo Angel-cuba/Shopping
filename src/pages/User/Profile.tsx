9d7eaad023dfdac554ac79464e8cfed43d7d00e0
Mac_Developer <araozangel842@gmail.com>
2023-04-11 15:38:02 +0300

    Ongoing work.
    Profile is available, with some default user data.  To add missing data to the profile, user has two differents options, one for the personal data and another for the payment data.

--- src/pages/User/Profile.tsx
+++ src/pages/User/Profile.tsx
@@ -1,41 +1,162 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../redux/store'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { UserType } from '../../interfaces/user/UserType';
+import {
+  Apartment,
+  CardMembershipTwoTone,
+  CardTravelTwoTone,
+  DateRange,
+  Email,
+  Numbers,
+  PasswordSharp,
+  Person,
+  Phone,
+  PostAdd,
+  PublicRounded,
+  StreetviewTwoTone,
+} from '@mui/icons-material';
+import ProfileForm from './ProfileForm';
+import './Profile.scss';
+import ProfilePayment from './ProfilePayment';
 
 const Profile = () => {
-  const { user } = useSelector((state: RootState) => state.userLogged)
+  const { user }: { user: UserType } = useSelector((state: RootState) => state.userLogged);
+  const [edit, setEdit] = React.useState(false);
+  const [editPayment, setEditPayment] = React.useState(false);
+  const [userEdited, setUserEdited] = React.useState<UserType>(user);
+
+  const handleOpenProfile = () => {
+    setEdit(!edit);
+    setEditPayment(false);
+  };
+  const handleOpenPayment = () => {
+    setEditPayment(!editPayment);
+    setEdit(false);
+  };
+  const iconStyles = {
+    width: '60%',
+    height: '100%',
+    color: '#6c6c6c',
+  };
 
   return (
     <div className="profile">
-      <div className="profile__container">
-        <div className="profile__container__header">
-          <h1>Welcome {user?.role === 'ADMIN' ? 'administrator' : ''}</h1>
-
-          <div className="profile__container__header__user">
-            <div className="profile__container__header__user__avatar">
-              <img
-                src={user?.picture}
-                alt={user?.name}
-                style={{
-                  width: '100px',
-                  height: '100px',
-                  borderRadius: '50%',
-                  objectFit: 'cover'
-                }}
-              />
+      <div className="profile__edit-button" onClick={handleOpenProfile}>
+        {edit ? 'Close editing view' : 'Edit profile'}
+      </div>
+      {edit && (
+        <div className="profile__edit-form">
+          <ProfileForm userEdited={userEdited} setUserEdited={setUserEdited} setEdit={setEdit} />
+        </div>
+      )}
+      {editPayment && (
+        <div className="profile__edit-form">
+          <ProfilePayment
+            userEdited={userEdited}
+            setUserEdited={setUserEdited}
+            setEditPayment={setEditPayment}
+          />
+        </div>
+      )}
+      <h1>Welcome {user.role === 'ADMIN' ? 'administrator' : ''}</h1>
+      <div className="profile__data">
+        <div className="profile__data__image-and-info">
+          <img
+            src={user?.picture}
+            alt={user?.name}
+            className="profile__data__image-and-info__image"
+          />
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <Person style={iconStyles} />
             </div>
-            <div className="profile__container__header__user__info">
-              <h2>{user?.name}</h2>
-              <p>{user?.email}</p>
+            {user.name}
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <PasswordSharp style={iconStyles} />
             </div>
+            ***********************
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <Email style={iconStyles} />
+            </div>
+            {user.email}
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <Phone style={iconStyles} />
+            </div>
+            +1234566778
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <StreetviewTwoTone style={iconStyles} />
+            </div>
+            Pääskynlento 99 Z1
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <Apartment style={iconStyles} />
+            </div>
+            Turku
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <PostAdd style={iconStyles} />
+            </div>
+            00100
+          </div>
+          <div className="profile__data__image-and-info__item">
+            <div className="profile__data__image-and-info__item--icon">
+              <PublicRounded style={iconStyles} />
+            </div>
+            Finland
           </div>
         </div>
-        <div className="profile__container__history">
-          <h2>History</h2>
+        <div className="profile__data__payment-info">
+          <div className="profile__data__payment-info__button" onClick={handleOpenPayment}>
+            Update payment information
+          </div>
+          <div className="profile__data__payment-info__item">
+            <div className="profile__data__payment-info__item--icon">
+              <Person style={iconStyles} />
+            </div>
+            John Doe
+          </div>
+          <div className="profile__data__payment-info__item">
+            <div className="profile__data__payment-info__item--icon">
+              <CardTravelTwoTone style={iconStyles} />
+            </div>
+            VISA
+          </div>
+          <div className="profile__data__payment-info__item">
+            <div className="profile__data__payment-info__item--icon">
+              <CardMembershipTwoTone style={iconStyles} />
+            </div>
+            Provider
+          </div>
+          <div className="profile__data__payment-info__item">
+            <div className="profile__data__payment-info__item--icon">
+              <Numbers style={iconStyles} />
+            </div>
+            1234 5678 9012 3456
+          </div>
+          <div className="profile__data__payment-info__item">
+            <div className="profile__data__payment-info__item--icon">
+              <DateRange style={iconStyles} />
+            </div>
+            12/22
+          </div>
         </div>
       </div>
+      <div className="profile__history">
+        <h2>History</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
---
+++ src/pages/User/ProfilePayment.tsx
@@ -0,0 +1,140 @@
+import React, { FormEvent } from 'react';
+import { UserType } from '../../interfaces/user/UserType';
+import { Input } from '../../components/Input/Input';
+
+type ProfilePaymentProps = {
+  userEdited: UserType;
+  setUserEdited: (userEdited: UserType) => void;
+  setEditPayment: (editPayment: boolean) => void;
+};
+const ProfilePayment = ({ userEdited, setUserEdited, setEditPayment }: ProfilePaymentProps) => {
+  const today = new Date();
+  const currentMonth = (today.getMonth() + 1).toString().padStart(2, '0');
+  const currentYear = today.getFullYear().toString();
+
+  const closeForm = () => {
+    setEditPayment(false);
+    setUserEdited({
+      ...userEdited,
+      cardHolder: '',
+      paymentType: '',
+      provider: '',
+      accountNumber: '',
+      expirationDate: '',
+    });
+  };
+  const handlerSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('handlerSubmit', userEdited);
+  };
+  return (
+    <div className="profile__edit-form__container">
+      <h1>ProfilePayment</h1>
+      <form onClick={handlerSubmit}>
+        <Input
+          type="text"
+          name="card name"
+          value={userEdited.cardHolder ?? ''}
+          onChange={(e) =>
+            setUserEdited({
+              ...userEdited,
+              cardHolder: e.target.value,
+            })
+          }
+          placeholder={!userEdited.cardHolder ? 'Full card name' : userEdited.cardHolder}
+          style={styles}
+          admin
+          profile
+        />
+        <div className="profile__edit-form__container__small">
+          <Input
+            type="text"
+            name="Type"
+            value={userEdited.paymentType ?? ''}
+            onChange={(e) =>
+              setUserEdited({
+                ...userEdited,
+                paymentType: e.target.value,
+              })
+            }
+            placeholder={!userEdited.paymentType ? 'Type' : userEdited.paymentType}
+            style={styleSmall}
+            small
+          />
+          <Input
+            type="text"
+            name="Provider"
+            value={userEdited.provider ?? ''}
+            onChange={(e) =>
+              setUserEdited({
+                ...userEdited,
+                provider: e.target.value,
+              })
+            }
+            placeholder={!userEdited.provider ? 'Provider' : userEdited.provider}
+            style={styleSmall}
+            small
+          />
+        </div>
+        <Input
+          type="text"
+          name="card number"
+          value={userEdited.accountNumber ?? ''}
+          onChange={(e) =>
+            setUserEdited({
+              ...userEdited,
+              accountNumber: e.target.value,
+            })
+          }
+          placeholder={!userEdited.accountNumber ? 'Full card number' : userEdited.accountNumber}
+          style={styles}
+          admin
+          profile
+        />
+        <Input
+          type="text"
+          name="expiration date"
+          value={userEdited.expirationDate ?? `${currentMonth}/${currentYear}`}
+          min={`${currentYear}-${currentMonth}`}
+          max={`${currentYear + 10}-12`}
+          onChange={(e) =>
+            setUserEdited({
+              ...userEdited,
+              expirationDate: e.target.value,
+            })
+          }
+          placeholder={!userEdited.expirationDate ? 'Expiration date' : userEdited.expirationDate}
+          style={styles}
+          admin
+          profile
+        />
+        <button type="submit" className="profile__edit-form__container__button">
+          Edit
+        </button>
+      </form>
+      <div className="profile__edit-form__container--cancel" onClick={closeForm}>
+        Cancel
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePayment;
+
+const styles = {
+  width: '280px',
+  height: '40px',
+  fontSize: '18px',
+  border: 'none',
+  borderRadius: '5px',
+  paddingLeft: '15px',
+};
+
+const styleSmall = {
+  width: '100px',
+  height: '40px',
+  fontSize: '18px',
+  border: 'none',
+  borderRadius: '5px',
+  paddingLeft: '15px',
+};
