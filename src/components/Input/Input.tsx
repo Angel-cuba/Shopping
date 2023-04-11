d06e0d3a0d19cf88610a18702552fd1ee0d5ca95
Mac_Developer <araozangel842@gmail.com>
2023-04-11 15:29:38 +0300

    Add props to increase usability

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,16 +5,28 @@ type InputProps = {
   name: string
   placeholder: string
   value: string | number
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   style?: { [key: string]: string }
   className?: string
-  // error: string,
-  // touched: boolean,
   message?: string
   admin?: boolean
+  profile?: boolean
+  small?: boolean
+  min?: string
+  max?: string
 }
 
-export const Input = ({ type, name, placeholder, value, onChange, style, admin }: InputProps) => {
+export const Input = ({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  style,
+  admin,
+  profile,
+  small
+}: InputProps) => {
   return (
     <div
       style={{
@@ -22,11 +34,19 @@ export const Input = ({ type, name, placeholder, value, onChange, style, admin }
         flexDirection: !admin ? 'column' : 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        width: admin ? '100%' : '200px',
+        width: admin ? '100%' : small ? '100px' : '200px',
         marginLeft: admin ? '0' : '20px',
-        padding: admin ? '10px' : '0'
+        padding: admin ? '10px' : '0',
+        borderRadius: '5px'
       }}>
-      <label style={{ color: '#313131', width: '80%', fontSize: '18px' }} htmlFor={name}>
+      <label
+        style={{
+          color: '#7e7e7e',
+          width: profile ? '40%' : small ? '100px' : '80%',
+          fontSize: '18px',
+          fontWeight: 'bold'
+        }}
+        htmlFor={name}>
         {name[0].toLocaleUpperCase() + name.slice(1)}
       </label>
       <input
