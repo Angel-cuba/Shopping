637687d87e3ac7dcea27e611ce155970354b825f
Mac_Developer <araozangel842@gmail.com>
2023-04-15 21:14:47 +0300

    Add components

---
+++ src/pages/Products/component/TrashItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useLottie } from 'lottie-react'
+import LottieItemTrash from '../../../assets/108969-delete.json'
+
+const TrashItem = () => {
+  const options = {
+    animationData: LottieItemTrash,
+    loop: true,
+    autoplay: true
+  }
+  const { View } = useLottie(options)
+  return <div className="products__content__item--animation-remove">{View}</div>
+}
+export default TrashItem
