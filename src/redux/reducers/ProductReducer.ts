1442791fcf8f9cb72e69a154fc311ff4c3283dde
Mac_Developer <araozangel842@gmail.com>
2023-04-19 12:14:01 +0300

    Change update method

--- src/redux/reducers/ProductReducer.ts
+++ src/redux/reducers/ProductReducer.ts
@@ -1,61 +1,69 @@
-import { AnyAction } from 'redux'
-import { ADD_PRODUCT, DELETE_PRODUCT, ERROR, GET_PRODUCTS, LOADING, ProductState, STOP_LOADING, UPDATE_PRODUCT } from '../../interfaces/products/constants'
-
+import { AnyAction } from 'redux';
+import {
+  ADD_PRODUCT,
+  DELETE_PRODUCT,
+  ERROR,
+  GET_PRODUCTS,
+  LOADING,
+  ProductState,
+  STOP_LOADING,
+  UPDATE_PRODUCT,
+} from '../../interfaces/products/constants';
 
 export const initialProductState: ProductState = {
   products: [],
   loading: false,
-  error: null
-}
+  error: null,
+};
 
 export default function productReducer(state = initialProductState, action: AnyAction) {
   switch (action.type) {
     case GET_PRODUCTS:
       return {
         ...state,
-        products: action.payload
-      }
+        products: action.payload,
+      };
     case ADD_PRODUCT: {
       return {
         ...state,
-        products: [action.payload, ...state.products]
-      }
+        products: [action.payload, ...state.products],
+      };
     }
     case UPDATE_PRODUCT: {
-      const product = state.products.find((product) => product.id === action.payload.id)
+      const products = state.products.map((product) => {
+        if (product.id === action.payload.id) {
+          return action.payload;
+        }
+        return product;
+      });
       return {
         ...state,
-        products: product
-          ? [
-              action.payload,
-              ...state.products.filter((product) => product.id !== action.payload.id)
-            ]
-          : state.products
-      }
+        products
+      };
     }
     case DELETE_PRODUCT: {
-      const removedProduct = state.products.filter((product) => product.id !== action.payload)
+      const removedProduct = state.products.filter((product) => product.id !== action.payload);
       return {
         ...state,
-        products: removedProduct
-      }
+        products: removedProduct,
+      };
     }
     case LOADING:
       return {
         ...state,
-        loading: true
-      }
+        loading: true,
+      };
     case STOP_LOADING:
       return {
         ...state,
-        loading: false
-      }
+        loading: false,
+      };
     case ERROR:
       return {
         ...state,
-        error: action.payload
-      }
+        error: action.payload,
+      };
     default:
-      return state
+      return state;
   }
 }
