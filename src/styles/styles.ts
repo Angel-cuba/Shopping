4e39975e22e1a968287067b8a4da5cfc2e3f2757
Mac_Developer <araozangel842@gmail.com>
2023-04-29 12:44:38 +0300

    Add global theme file

---
+++ src/styles/styles.ts
@@ -0,0 +1,19 @@
+export const darkTheme = {
+  bg: '#2C3333',
+  color: '#2E4F4F',
+  primary: '#0E8388',
+  secondary: '#CBE4DE',
+  grey: '#666',
+  shadow: '#8B8B8B0F',
+  textLink: '#2A2A2A',
+};
+
+export const lightTheme = {
+  bg: '#FDFDFD',
+  color: '#F7E1AE',
+  primary: '#A4D0A4',
+  secondary: '#617A55',
+  grey: '#333',
+  shadow: '#0000000F',
+  textLink: '#ECECEC',
+};
