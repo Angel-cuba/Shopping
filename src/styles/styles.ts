6954af0d1e602bca07d21751445ff4ff87e6be5f
Mac_Developer <araozangel842@gmail.com>
2023-04-30 00:49:18 +0300

    Add new values

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,19 +1,21 @@
 export const darkTheme = {
   bg: '#2C3333',
-  color: '#2E4F4F',
+  itemBg: '#F7E1AE',
   primary: '#0E8388',
   secondary: '#CBE4DE',
   grey: '#666',
   shadow: '#8B8B8B0F',
   textLink: '#2A2A2A',
+  textItem: '#2A2C2C',
 };
 
 export const lightTheme = {
   bg: '#FDFDFD',
-  color: '#F7E1AE',
+  itemBg: '#ECECEC',
   primary: '#A4D0A4',
   secondary: '#617A55',
   grey: '#333',
   shadow: '#0000000F',
   textLink: '#ECECEC',
+  textItem: '#282C2D',
 };
