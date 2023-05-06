1fab437e55f451365fd85b446b335464bc8effa3
Mac_Developer <araozangel842@gmail.com>
2023-05-06 20:32:15 +0300

    Adding new component.
    This component returns few products without anything, just is simulating the incoming data. Also has some styles to cover the spaces where this data is being displayed.

---
+++ src/components/Loading/LoadingProducts.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import './LoadingProducts.scss';
+import { MoreVertOutlined } from '@mui/icons-material';
+
+const LoadingProducts = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>
+            <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>      <div className="loading-container__item">
+        Loading...
+        <div className="loading-container__item--details">
+          <MoreVertOutlined />
+          Details
+        </div>
+        <div className="loading-container__item--animation-add"></div>
+        <div className="loading-container__item--image"></div>
+        <div className="loading-container__item--info">
+          <div className="loading-container__item--info--name">Name</div>
+          <div className="loading-container__item--info--description">Description</div>
+          <div className="loading-container__item--info--price">Price</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingProducts;
