a4c61f6acc5eadbde35c65ad5b12038a0dfe1a28
Mac_Developer <araozangel842@gmail.com>
2023-05-08 03:36:34 +0300

    Add new types.

--- src/interfaces/products/constants.ts
+++ src/interfaces/products/constants.ts
@@ -1,51 +1,73 @@
-import { Product } from "./ProductType"
+import { Product } from './ProductType';
 
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 
-export const GET_PRODUCTS = 'GET_PRODUCTS'
-export const ADD_PRODUCT = 'ADD_PRODUCT'
-export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
-export const DELETE_PRODUCT = 'DELETE_PRODUCT'
-
-export const LOADING = 'LOADING'
-export const STOP_LOADING = 'STOP_LOADING'
-export const ERROR = 'ERROR'
+export const LOADING = 'LOADING';
+export const STOP_LOADING = 'STOP_LOADING';
+export const REQUEST = 'REQUEST'
+export const SUCCESSFUL = 'SUCCESSFUL';
+export const FAILURE = 'FAILURE';
+export const ERROR = 'ERROR';
 
 //Action types for product
 export type LoadingAction = {
-  type: typeof LOADING
-}
+  type: typeof LOADING;
+};
 export type StopLoadingAction = {
-  type: typeof STOP_LOADING
-}
+  type: typeof STOP_LOADING;
+};
 export type ErrorAction = {
-  type: typeof ERROR
-  payload: string | null
-}
+  type: typeof ERROR;
+  payload: string | null;
+};
 
 export type GetProductsAction = {
-  type: typeof GET_PRODUCTS
-  payload: Product[]
-}
+  type: typeof GET_PRODUCTS;
+  payload: Product[];
+};
 
 export type AddProductAction = {
-  type: typeof ADD_PRODUCT
-  payload: Product
-}
+  type: typeof ADD_PRODUCT;
+  payload: Product;
+};
 
 export type UpdateProductAction = {
-  type: typeof UPDATE_PRODUCT
-  payload: Product
-}
+  type: typeof UPDATE_PRODUCT;
+  payload: Product;
+};
 
 export type DeleteProductAction = {
-  type: typeof DELETE_PRODUCT
-  payload: string
+  type: typeof DELETE_PRODUCT;
+  payload: string;
+};
+
+export type RequestAction = {
+  type: typeof REQUEST;
 }
 
-export type ProductActionTypes = GetProductsAction | LoadingAction | StopLoadingAction | ErrorAction
+export type SuccessFulAction = {
+  type: typeof SUCCESSFUL;
+};
+
+export type FailureAction = {
+  type: typeof FAILURE;
+};
+
+export type ProductActionTypes =
+  | GetProductsAction
+  | LoadingAction
+  | StopLoadingAction
+  | ErrorAction
+  | RequestAction
+  | SuccessFulAction
+  | FailureAction;
 
 export type ProductState = {
-  products: Product[]
-  loading: boolean
-  error: string | null
-}
+  products: Product[];
+  loading: boolean;
+  error: string | null;
+  success: boolean;
+};
