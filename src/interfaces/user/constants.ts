5d0a513966545612a0c2f7e7f518a111d009ea2c
Mac_Developer <araozangel842@gmail.com>
2023-05-14 13:42:02 +0300

    Add new action and update imported type

--- src/interfaces/user/constants.ts
+++ src/interfaces/user/constants.ts
@@ -1,6 +1,8 @@
-import { UserType } from './UserType'
+import { DecodedUser } from '../../utils/type-guards'
+import {  UserType } from './UserType'
 
 export const LOGGED_IN = 'LOGGED_IN'
+export const LOGGED = 'LOGGED'
 export const LOGGED_OUT = 'LOGGED_OUT'
 
 export type LoggedInAction = {
@@ -8,13 +10,19 @@ export type LoggedInAction = {
   payload: UserType
 }
 
+export type LoggedAction = {
+  type: typeof LOGGED
+  payload: DecodedUser
+}
+
 export type LoggedOutAction = {
   type: typeof LOGGED_OUT
   payload: null
 }
 
-export type UserActionTypes = LoggedInAction | LoggedOutAction
+export type UserActionTypes = LoggedInAction | LoggedAction | LoggedOutAction
 
 export type UserState = {
   user: UserType | null
+  userFromToken: DecodedUser | null
 }
