cb6af48e4e37ee44965cda74758f566861fbc64f
Mac_Developer <araozangel842@gmail.com>
2023-05-21 19:11:56 +0300

    Add types and constants for wishes

---
+++ src/interfaces/wishes/WishesType.ts
@@ -0,0 +1,3 @@
+export type Wishes = {
+  string: string[];
+};
--- src/interfaces/wishes/constants.ts
+++ src/interfaces/wishes/constants.ts
@@ -1,21 +1,28 @@
-import { Product } from "../products/ProductType"
-
-
+export const GET_WISHLIST = 'GET_WISHLIST'
 export const ADD_TO_WISHLIST = 'ADD_TO_WISHLIST'
+export const ADD_TO_WISHLIST_SUCCESS = 'ADD_TO_WISHLIST_SUCCESS'
+export const ADD_TO_WISHLIST_FAILURE = 'ADD_TO_WISHLIST_FAILURE'
 export const REMOVE_FROM_WISHLIST = 'REMOVE_FROM_WISHLIST'
+export const CLEAR_WISHLIST = 'CLEAR_WISHLIST'
+
+
+export type GetWishlistAction = {
+  type: typeof GET_WISHLIST
+  payload: string[]
+}
 
 export type AddToWishlistAction = {
   type: typeof ADD_TO_WISHLIST
-  payload: Product
+  payload: string
 }
 
 export type RemoveFromWishlistAction = {
   type: typeof REMOVE_FROM_WISHLIST
-  payload: Product
+  payload: string
 }
 
 export type WishlistActionTypes = AddToWishlistAction | RemoveFromWishlistAction
 
 export type WishListState = {
-  itemInWishlist: Product[] | null
+  itemInWishlist: string[] | null
 }
