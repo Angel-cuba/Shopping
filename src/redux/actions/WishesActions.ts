17ef2133cbf135684dbf4571ff97f7094b5de696
Mac_Developer <araozangel842@gmail.com>
2023-05-22 18:54:29 +0300

    Implementing the wish list feature
    On WishesActions.ts:
    1- Fetch the wish list from the database of the specified user.
    2- Create a wish list for the user if it doesn't exist.
    3- Add and remove a wish(product) from the wish list.
    4- Delete the wish list if there is only one wish in the list and the user removes it.
    
    On WishesReducer.ts:
    1- Adding more cases to the reducer to handle the new actions.
    2- Send the wish list to the database after adding or removing a wish.
    3- Update the state with the new wish list after adding or removing a wish.

--- src/redux/actions/WishesActions.ts
+++ src/redux/actions/WishesActions.ts
@@ -1,36 +1,132 @@
-import { Dispatch } from "redux"
-import { Wishes } from "../../interfaces/wishes/WishesType"
-import { ADD_TO_WISHLIST, GET_WISHLIST, REMOVE_FROM_WISHLIST } from "../../interfaces/wishes/constants"
+import { Dispatch } from 'redux';
+import { Wishes } from '../../interfaces/wishes/WishesType';
+import {
+  ADD_TO_WISHLIST,
+  CLEAR_WISHLIST,
+  ERROR_WISHES,
+  GET_WISHLIST,
+  LOADING_WISHES,
+  REMOVE_FROM_WISHLIST,
+  STOP_LOADING_WISHES,
+} from '../../interfaces/wishes/constants';
 import { api } from '../../utils/api';
 
 export const getWishes = (wishesList: Wishes) => {
   return {
     type: GET_WISHLIST,
-    payload: wishesList
-  } as const
-}
+    payload: wishesList,
+  } as const;
+};
 
 export const addToWishList = (productId: string) => {
   return {
     type: ADD_TO_WISHLIST,
-    payload: productId
-  } as const
-}
+    payload: productId,
+  } as const;
+};
 
 export const removeFromWishList = (productId: string) => {
   return {
     type: REMOVE_FROM_WISHLIST,
-    payload: productId
-  } as const
-}
+    payload: productId,
+  } as const;
+};
+
+export const clearWishList = (emptyList: string) => {
+  return {
+    type: CLEAR_WISHLIST,
+  } as const;
+};
 
 export const getWishList = (id: string) => {
   return async (dispatch: Dispatch) => {
     try {
-      const response = await api.get(`/wishes/user/${id}`)
-      dispatch(getWishes(response.data[0].userWishes))
+      const response = await api.get(`/wishes/user/${id}`);
+      dispatch(getWishes(response.data[0]?.userWishes));
     } catch (error) {
-      console.log(error)
+      dispatch({ type: ERROR_WISHES, payload: error });
     }
-  }
-}
+  };
+};
+
+export const createWishList = (id: string, userId: string) => {
+  return async (dispatch: Dispatch) => {
+    const data = {
+      userWishes: [id],
+      totalOfItems: 1,
+      user: {
+        id: userId,
+      },
+    };
+    dispatch({ type: LOADING_WISHES });
+    try {
+      const response = await api.post('/wishes', data);
+      dispatch(addToWishList(response.data));
+    } catch (error) {
+      dispatch({ type: ERROR_WISHES, payload: error });
+    }
+    dispatch({ type: STOP_LOADING_WISHES });
+  };
+};
+
+export const addingToWishList = (id: string, userId: string) => {
+  return async (dispatch: Dispatch) => {
+    dispatch({ type: LOADING_WISHES });
+    try {
+      const checkIfExist = await api.get(`/wishes/user/${userId}`);
+      const wishesList = [...checkIfExist.data[0].userWishes, id];
+      const dataToUpdate = {
+        id: checkIfExist.data[0].id,
+        userWishes: wishesList,
+        totalOfItems: checkIfExist.data[0].totalOfItems + 1,
+        user: {
+          id: userId,
+        },
+      };
+      const response = await api.put('/wishes', dataToUpdate);
+      dispatch(addToWishList(response.data));
+    } catch (error) {
+      dispatch({ type: ERROR_WISHES, payload: error });
+    }
+    dispatch({ type: STOP_LOADING_WISHES });
+  };
+};
+
+export const removingFromWishList = (id: string, userId: string) => {
+  return async (dispatch: Dispatch) => {
+    dispatch({ type: LOADING_WISHES });
+    try {
+      const checkIfExist = await api.get(`/wishes/user/${userId}`);
+      const wishesList = checkIfExist.data[0].userWishes.filter((wish: string) => wish !== id);
+      const dataToUpdate = {
+        id: checkIfExist.data[0].id,
+        userWishes: wishesList,
+        totalOfItems: checkIfExist.data[0].totalOfItems - 1,
+        user: {
+          id: userId,
+        },
+      };
+      const response = await api.put('/wishes', dataToUpdate);
+      dispatch(removeFromWishList(response.data));
+    } catch (error) {
+      dispatch({ type: ERROR_WISHES, payload: error });
+    }
+    dispatch({ type: STOP_LOADING_WISHES });
+  };
+};
+
+export const deleteWishList = (userId: string) => {
+  return async (dispatch: Dispatch) => {
+    dispatch({ type: LOADING_WISHES });
+    try {
+      const checkIfExist = await api.get(`/wishes/user/${userId}`);
+      if (checkIfExist.status === 200) {
+        const response = await api.delete(`/wishes/${checkIfExist.data[0].id}`);
+        dispatch(clearWishList(response.data));
+      }
+    } catch (error) {
+      dispatch({ type: ERROR_WISHES, payload: error });
+    }
+    dispatch({ type: STOP_LOADING_WISHES });
+  };
+};
--- src/redux/reducers/WishesReducer.ts
+++ src/redux/reducers/WishesReducer.ts
@@ -1,17 +1,45 @@
 import { AnyAction } from 'redux';
 import {
   ADD_TO_WISHLIST,
+  CLEAR_WISHLIST,
   GET_WISHLIST,
+  LOADING_WISHES,
   REMOVE_FROM_WISHLIST,
+  REQUEST_WISHES,
+  STOP_LOADING_WISHES,
+  SUCCESSFUL_WISHES,
   WishListState,
 } from '../../interfaces/wishes/constants';
 
 export const initialWishState: WishListState = {
   itemInWishlist: [],
+  loadingWishes: false,
+  errorWishes: null,
+  successWishes: false,
 };
 
 export default function wishReducer(state = initialWishState, action: AnyAction) {
   switch (action.type) {
+    case LOADING_WISHES:
+      return {
+        ...state,
+        loading: true,
+      };
+    case STOP_LOADING_WISHES:
+      return {
+        ...state,
+        loading: false,
+      };
+    case REQUEST_WISHES:
+      return {
+        ...state,
+        success: true,
+      };
+    case SUCCESSFUL_WISHES:
+      return {
+        ...state,
+        success: false,
+      };
     case GET_WISHLIST:
       return {
         ...state,
@@ -26,7 +54,7 @@ export default function wishReducer(state = initialWishState, action: AnyAction)
       }
       return {
         ...state,
-        itemInWishlist: [...state.itemInWishlist, action.payload],
+        itemInWishlist: action.payload.userWishes,
       };
     }
     case REMOVE_FROM_WISHLIST: {
@@ -35,9 +63,14 @@ export default function wishReducer(state = initialWishState, action: AnyAction)
       }
       return {
         ...state,
-        itemInWishlist: state.itemInWishlist?.filter((item) => item !== action.payload),
+        itemInWishlist: action.payload.userWishes,
       };
     }
+    case CLEAR_WISHLIST:
+      return {
+        ...state,
+        itemInWishlist: action.payload,
+      };
     default:
       return state;
   }
