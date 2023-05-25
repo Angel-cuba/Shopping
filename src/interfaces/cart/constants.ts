9054dce0f29f95ac3411f6f7e4d01151c7ab9de8
Mac_Developer <araozangel842@gmail.com>
2023-05-25 03:18:43 +0300

    Add clear cart constant

--- src/interfaces/cart/constants.ts
+++ src/interfaces/cart/constants.ts
@@ -2,6 +2,7 @@ import { CartProduct } from './CartType'
 
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+export const CLEAR_CART = 'CLEAR_CART'
 
 export type AddToCartAction = {
   type: typeof ADD_TO_CART
