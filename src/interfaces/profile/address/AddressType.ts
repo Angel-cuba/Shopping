7b63c5a40e9e4986f1ebceb45db21ba13417c41e
Mac_Developer <araozangel842@gmail.com>
2023-05-25 17:41:16 +0300

    Adding types and constants for address and payment

---
+++ src/interfaces/profile/address/AddressType.ts
@@ -0,0 +1,9 @@
+export type AddressType = {
+  id?: string 
+  address: string
+  city: string
+  country: string
+  postalCode: string
+  createdAt: string
+  updatedAt: string
+  }
---
+++ src/interfaces/profile/payment/paymentType.ts
@@ -0,0 +1,10 @@
+export type PaymentType = {
+  id?: string
+  cardHolderName: string
+  cardNumber: string
+  expirationDate: string
+  createdAt: string
+  updatedAt: string
+  paymentType: string
+  provider: string 
+}
