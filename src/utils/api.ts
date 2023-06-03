42951e4802b73965df1837f6edfd965816e4cd24
Mac_Developer <araozangel842@gmail.com>
2023-06-04 02:22:08 +0300

    Refactor axios instance

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,10 +2,19 @@ import axios from 'axios';
 import { getTokenFromLocalStorage } from './token';
 
 const token = getTokenFromLocalStorage();
-
+let baseURL: string
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  baseURL= 'http://localhost:8080/api/v1'
+} else {
+  baseURL= 'https://shopping-bhjf.onrender.com/api/v1'
+}
 export const api = axios.create({
-  baseURL: 'https://shopping-bhjf.onrender.com/api/v1',
+  baseURL,
   headers: {
     Authorization: `Bearer ${token}`,
   },
+});
+
+export const apiWithoutAuth = axios.create({
+  baseURL,
 });
