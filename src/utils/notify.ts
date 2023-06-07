83fa550ba7df25895145d617ec8eedb2bb418ced
Mac_Developer <araozangel842@gmail.com>
2023-06-08 00:19:17 +0300

    Adding new notifier method.
    Run prettier.

--- src/utils/notify.ts
+++ src/utils/notify.ts
@@ -1,6 +1,7 @@
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
 
-export const notifyDelete = (message: string) => toast.error(message, {
+export const notifyDelete = (message: string) =>
+  toast.error(message, {
     position: toast.POSITION.TOP_LEFT,
     autoClose: 1600,
     style: {
@@ -8,11 +9,12 @@ export const notifyDelete = (message: string) => toast.error(message, {
       color: '#fff',
       fontWeight: 'bold',
       borderRadius: '5px',
-      textAlign: 'center'
-    }, 
+      textAlign: 'center',
+    },
   });
 
-  export const notifyWarning = (message: string) => toast.error(message, {
+export const notifyWarning = (message: string) =>
+  toast.error(message, {
     position: toast.POSITION.TOP_CENTER,
     autoClose: 1600,
     style: {
@@ -20,56 +22,66 @@ export const notifyDelete = (message: string) => toast.error(message, {
       color: '#fff',
       fontWeight: 'bold',
       borderRadius: '5px',
-      textAlign: 'center'
-    }, 
+      textAlign: 'center',
+    },
   });
 
-
-  export const notifyEmptyFields = (message: string) => toast.error(message, {
+export const notifyEmptyFields = (message: string) =>
+  toast.error(message, {
     position: toast.POSITION.TOP_CENTER,
     icon: '❌',
     autoClose: 2000,
   });
 
-  export const notifyError = (error: string) => toast.error(error, {
+export const notifyError = (error: string) =>
+  toast.error(error, {
     position: toast.POSITION.TOP_RIGHT,
     icon: '❌',
     autoClose: 1600,
   });
 
-  export const notifySuccess = (message: string) => toast.success(message, {
+export const notifySuccess = (message: string) =>
+  toast.success(message, {
     position: toast.POSITION.TOP_RIGHT,
     icon: '🚀',
     autoClose: 1600,
   });
 
-  export const notifyRedirectToLogin = () => toast.success('You have been redirected to login', {
+export const notifyRedirect = () =>
+  toast.success('You have been redirected to home', {
     position: toast.POSITION.TOP_CENTER,
-    icon: '🚀',
+    autoClose: 2000,
+    style: {
+      textAlign: 'center',
+      fontWeight: 'bold',
+      backgroundColor: '#000000D3',
+      color: '#d3d3d3'
+    },
+  });
+
+export const notifyRedirectToProfile = () =>
+  toast.success('You have been redirected to profile', {
+    position: toast.POSITION.TOP_CENTER,
     autoClose: 2000,
   });
 
-  export const notifyRedirectToProfile = () => toast.success('You have been redirected to profile', {
-    position: toast.POSITION.TOP_CENTER,
-    autoClose: 2000,
-  });
-
-
-  export const notifyRedirectToHome = (name: string) => toast.success(`Welcome back ${name}, you have been redirected`, {
+export const notifyRedirectToHome = (name: string) =>
+  toast.success(`Welcome back ${name}, you have been redirected`, {
     position: toast.POSITION.TOP_LEFT,
     icon: '🚀',
     autoClose: 2000,
   });
 
-  export const notifyRedirectToHomeAfterRegister = (name: string) => toast.success(`Welcome ${name}, you have been redirected`, {
+export const notifyRedirectToHomeAfterRegister = (name: string) =>
+  toast.success(`Welcome ${name}, you have been redirected`, {
     position: toast.POSITION.TOP_LEFT,
     icon: '🚀',
     autoClose: 2000,
   });
 
-  export const notifyCredentialsAcepeted = () => toast.success('Credentials accepted', {
+export const notifyCredentialsAcepeted = () =>
+  toast.success('Credentials accepted', {
     position: toast.POSITION.TOP_CENTER,
     icon: '📡',
     autoClose: 2000,
   });
-
