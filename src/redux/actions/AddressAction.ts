e8fc0006938cd9d22f97f4f4bb0189124a0d1ef5
Mac_Developer <araozangel842@gmail.com>
2023-06-23 21:34:18 +0300

    Add method and action to handle payment requests

--- src/redux/actions/AddressAction.ts
+++ src/redux/actions/AddressAction.ts
@@ -1,24 +1,96 @@
-import { Dispatch } from "redux";
-import { api } from "../../utils/api";
-import { FETCH_ALL_ADDRESS } from "../../interfaces/profile/address/constants";
-import { AddressType } from "../../interfaces/profile/address/AddressType";
-
+import { Dispatch } from 'redux';
+import { api } from '../../utils/api';
+import {
+  ADD_ADDRESS,
+  DELETE_ADDRESS,
+  ERROR,
+  FETCH_ALL_ADDRESS,
+  LOADING,
+  STOP_LOADING,
+  UPDATE_ADDRESS,
+} from '../../interfaces/profile/address/constants';
+import { AddressToSend, AddressType } from '../../interfaces/profile/address/AddressType';
 
 export const getAddresses = (addresses: AddressType) => {
- return {
-  type: FETCH_ALL_ADDRESS,
-  payload: addresses
- }
-}
+  return {
+    type: FETCH_ALL_ADDRESS,
+    payload: addresses,
+  };
+};
 
+export const addAddress = (address: AddressToSend) => {
+  return {
+    type: ADD_ADDRESS,
+    payload: address,
+  }as const;
+};
 
-export const fetchingAddresses = (userId: string ) => {
+export const updateAddress = (address: AddressToSend) => {
+  return {
+    type: UPDATE_ADDRESS,
+    payload: address,
+  };
+};
+
+export const deleteAddress = (addressId: string) => {
+  return {
+    type: DELETE_ADDRESS,
+    payload: addressId,
+  };
+};
+
+export const fetchingAddresses = (userId: string) => {
   return async (dispatch: Dispatch) => {
     try {
-    const response = await api.get(`/addresses/user/${userId}`)
-    dispatch(getAddresses(response.data));
+      dispatch({ type: LOADING });
+      const response = await api.get(`/addresses/user/${userId}`);
+      if (response.status !== 200) {
+        dispatch({ type: ERROR, payload: response.data });
+      } else {
+        dispatch(getAddresses(response.data));
+      }
     } catch (error) {
-    console.log(error)
+      dispatch({ type: ERROR, payload: error });
     }
+    dispatch({ type: STOP_LOADING });
+  };
+};
+
+export const addingAddress = (address: AddressToSend) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({type:LOADING})
+      const request = await api.post('/addresses', address)
+      dispatch(addAddress(request.data))
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error})
+    }
+    dispatch({ type: STOP_LOADING })
   }
+}
+
+export const updatingAddress = (address: AddressToSend) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({type:LOADING})
+      const request = await api.put(`/addresses`, address)
+      dispatch(updateAddress(request.data))
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error})
+    }
+    dispatch({ type: STOP_LOADING })
   }
+}
+
+export const deletingAddress = (addressId: string) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({type:LOADING})
+      await api.delete(`/addresses/${addressId}`)
+      dispatch(deleteAddress(addressId))
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error})
+    }
+    dispatch({ type: STOP_LOADING })
+  }
+}
--- src/redux/reducers/AddressReducer.ts
+++ src/redux/reducers/AddressReducer.ts
@@ -1,12 +1,18 @@
 import { AnyAction } from 'redux';
-import { AddressState, FETCH_ALL_ADDRESS } from '../../interfaces/profile/address/constants';
+import {
+  ADD_ADDRESS,
+  AddressState,
+  DELETE_ADDRESS,
+  FETCH_ALL_ADDRESS,
+  UPDATE_ADDRESS,
+} from '../../interfaces/profile/address/constants';
 
 export const initialAddressState: AddressState = {
   addresses: [],
   loading: false,
   error: null,
-  success: false
-}
+  success: false,
+};
 
 export default function addressReducer(state = initialAddressState, action: AnyAction) {
   switch (action.type) {
@@ -16,6 +22,31 @@ export default function addressReducer(state = initialAddressState, action: AnyA
         addresses: action.payload,
       };
     }
+
+    case ADD_ADDRESS:
+      return {
+        ...state,
+        addresses: [...state.addresses, action.payload],
+      };
+
+    case UPDATE_ADDRESS:
+      const addresses = state.addresses.map((address) => {
+        if (address.id === action.payload.id) {
+          return action.payload;
+        }
+        return address;
+      });
+      return {
+        ...state,
+        addresses,
+      };
+
+    case DELETE_ADDRESS:
+      const removedAddress = state.addresses.filter((address) => address.id !== action.payload);
+      return {
+        ...state,
+        addresses: removedAddress,
+      };
 
     default:
       return state;
