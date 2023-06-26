19378143e7a6934f2ba1884531d3b687de894a40
Mac_Developer <araozangel842@gmail.com>
2023-06-26 23:12:52 +0300

    Ongoing work
    1- Add type to Order payment object.
    2- Working in UI/UX.
    3- Adding styles.

--- src/pages/User/History/UserHistory.tsx
+++ src/pages/User/History/UserHistory.tsx
@@ -1,40 +1,56 @@
 import React, { useEffect } from 'react';
+import moment from 'moment';
+import './UserHistory.scss';
+import { api } from '../../../utils/api';
 
 type Props = {
   setOpenHistory: (opened: boolean) => void;
-  history: any;
+  history: History[] | undefined;
+};
+
+type History = {
+  id: string;
+  paymentType: string;
+  shippingMethod: string;
+  shippingAddress: string;
+  shippingFee: number;
+  total: number;
+  createdAt: string;
+  orderDetails: string[];
+};
+type date = {
+  dateString: string;
+};
+type orderDetails = {
+  orderDetails: any;
 };
 
 const UserHistory = ({ setOpenHistory, history }: Props) => {
-  // console.log("🚀 ~ file: UserHistory.tsx:10 ~ UserHistory ~ history:", history)
+  console.log('🚀 ~ file: UserHistory.tsx:10 ~ UserHistory ~ history:', history);
 
   const closeHistory = () => {
     setOpenHistory(false);
   };
   return (
     <div className="history">
-      <button onClick={closeHistory}>Close</button>
-      {history?.map((order: any) => (
-        <div key={order.id}>
-          <div>{order.id}</div>
-          <div>{order.createdAt}</div>
-          <div>{order.paymentType}</div>
-          <div>{order.shippingAddress}</div>
-          <div>{order.shippingMethod}</div>
-          <div>{order.shippingFee}</div>
-          <div>{order.totalPrice}</div>
-          {/* <div>{order.orderDetails.map((item: any) => (
-              <div key={item.id}>
-                <div>{item.id}</div>
-                <div>{item.variant}</div>
-                <div>{item.image}</div>
-                <div>{item.size}</div>
-                <div>{item.price}</div>
-                <div>{item.quantity}</div>
-                <div>{item.user.id}</div>
-              </div>
-            ))}</div> */}
-          <OrderDetails orderDetails={order.orderDetails} />
+      <button onClick={closeHistory} className="history__btn">
+        Close
+      </button>
+      {history?.map((order: History) => (
+        <div key={order.id} className="history__item">
+          <div className="history__item--type">{order.paymentType}</div>
+          <div className="history__item--address">Delivered in: {order.shippingAddress}</div>
+          <div className="history__item--method">
+            {order.shippingMethod === 'DOOR' ? 'Received at home' : 'Picked up from store'}
+          </div>
+          <div className="history__item--total">Total spent ${order.total}</div>
+          <div className="history__item--date">
+            <Date dateString={order.createdAt} />
+          </div>
+          <div className="history__item__details">
+            <h2>Details</h2>
+            <OrderDetails orderDetails={order.orderDetails} />
+          </div>
         </div>
       ))}
     </div>
@@ -43,21 +59,33 @@ const UserHistory = ({ setOpenHistory, history }: Props) => {
 
 export default UserHistory;
 
-const OrderDetails = ({ orderDetails }: any) => {
+const OrderDetails = ({ orderDetails }: orderDetails) => {
   console.log('🚀 ~ file: UserHistory.tsx:51 ~ OrderDetails ~ orderDetails:', orderDetails);
   // const [details, setDetails] = React.useState<any>([])
   // console.log("🚀 ~ file: UserHistory.tsx:51 ~ OrderDetails ~ details:", details)
   // React.useEffect(() => {
 
-  // const fetchingData = async () => {
+
   useEffect(() => {
-    const request = async () => {
-      const response = await fetch(`/order-details/user/?orderDetailsIds=[${orderDetails}]`);
-      const data = await response.json();
-      console.log('🚀 ~ file: UserHistory.tsx:61 ~ request ~ response:', response);
-      console.log('🚀 ~ file: UserHistory.tsx:61 ~ fetchingData ~ data:', data);
+    // const request = async () => {
+    //   const response = await api.get('/order-details/all-order-details', orderDetails)
+    //   console.log("🚀 ~ file: UserHistory.tsx:80 ~ request ~ response:", response)
+
+    // };
+    // request();
+      const enviarSolicitud = async () => {
+      try {
+        const params = new URLSearchParams();
+        orderDetails.forEach((item: any) => params.append('orderDetailsIds', item));
+
+        const response = await api.get('/order-details/user-order-details', { params: params });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     };
-    request();
+
+    enviarSolicitud();
   }, [orderDetails]);
 
   // setDetails(request.data)
@@ -78,5 +106,18 @@ const OrderDetails = ({ orderDetails }: any) => {
         </div>
       ))} */}
     </div>
+  );
+};
+
+const Date = ({ dateString }: date) => {
+  const date = moment(dateString);
+  const dayOfWeek = date.format('dddd');
+  const formattedDate = date.format('YYYY-MM-DD');
+  const time = date.format('HH:mm:ss');
+
+  return (
+    <>
+      {dayOfWeek}, {formattedDate} at {time}
+    </>
   );
 };
